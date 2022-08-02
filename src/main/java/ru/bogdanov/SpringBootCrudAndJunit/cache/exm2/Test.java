1bae941f1af7db8aa72852a16b1159cb24ae3b9e
Bogdanov Maxim <bogdanovmw127@gmail.com>
2022-08-02 18:35:19 +0500

    Test commit add 2

--- src/main/java/ru/bogdanov/SpringBootCrudAndJunit/cache/exm2/Test.java
+++ src/main/java/ru/bogdanov/SpringBootCrudAndJunit/cache/exm2/Test.java
@@ -2,6 +2,6 @@ package ru.bogdanov.SpringBootCrudAndJunit.cache.exm2;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Привет");
+        System.out.println("ПРиве 2");
     }
 }
